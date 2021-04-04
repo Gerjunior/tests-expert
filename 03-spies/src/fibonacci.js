09be0d4a6769dc2a442bd16318921171ccbf00b9
Geraldo Júnior <geraldojuni@hotmail.com>
2021-04-04 17:12:33 -0300

    ✨ feat: spies

---
+++ 03-spies/src/fibonacci.js
@@ -0,0 +1,11 @@
+export default class Fibonacci {
+  // generator
+  *execute(input, current = 0, next = 1) {
+    if (input === 0) {
+      return 0
+    }
+
+    yield current
+    yield* this.execute(input - 1, next, current + next)
+  }
+}
