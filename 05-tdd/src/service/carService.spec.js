16ff2a0df5de35d44fb6e21a9f375f2f1df80156
Geraldo Júnior <geraldojuni@hotmail.com>
2021-04-18 22:04:05 -0300

    ✨ feat: TDD use case 3

--- 05-tdd/src/service/carService.spec.js
+++ 05-tdd/src/service/carService.spec.js
@@ -4,7 +4,9 @@ import { fileURLToPath } from 'url'
 import { expect } from 'chai'
 import sinon from 'sinon'
 
+
 import CarService from '../../src/service/carService.js'
+import Transaction from '../../src/entities/transaction.js'
 
 const carsDatabase = join(fileURLToPath(import.meta.url), './../../../database', 'cars.json')
 
@@ -29,7 +31,7 @@ describe('CarService Suite Tests', () => {
   })
 
   afterEach(() => {
-      sandBox.restore()
+    sandBox.restore()
   })
 
   it('should retrieve a random position from an array', () => {
@@ -44,10 +46,7 @@ describe('CarService Suite Tests', () => {
     const carCategory = mocks.validCarCategory
     const carIdIndex = 0
 
-    sandBox.stub(
-      carService,
-      carService.getRandomPositionFromArray.name
-    ).returns(carIdIndex)
+    sandBox.stub(carService, carService.getRandomPositionFromArray.name).returns(carIdIndex)
 
     const result = carService.chooseRandomCar(carCategory)
     const expected = carCategory.carIds[carIdIndex]
@@ -58,23 +57,64 @@ describe('CarService Suite Tests', () => {
 
   it('given a carCategory it should return an available car', async () => {
     const car = mocks.validCar
-    const carCategory = { ...mocks.validCarCategory, carIds: [car.id]}
+    const carCategory = { ...mocks.validCarCategory, carIds: [car.id] }
 
-    sandBox.stub(
-      carService.carRepository,
-      carService.carRepository.find.name,
-    ).resolves(car)
+    sandBox.stub(carService.carRepository, carService.carRepository.find.name).resolves(car)
 
-    sandBox.spy(
-      carService,
-      carService.chooseRandomCar.name
-    )
+    sandBox.spy(carService, carService.chooseRandomCar.name)
 
     const result = await carService.getAvailableCar(carCategory)
     const expected = car
 
     expect(carService.chooseRandomCar.calledOnce).to.be.ok
     expect(carService.carRepository.find.calledWithExactly(car.id)).to.be.ok
+    expect(result).to.be.deep.equal(expected)
+  })
+
+  it('given a carCategory, customer and numberOfDays it should calculate final amount in real', async () => {
+    const customer = { ...mocks.validCustomer, age: 50 }
+    const carCategory = { ...mocks.validCarCategory, price: 37.6 }
+
+    const numberOfDays = 5
+
+    sandBox.stub(carService, 'taxesBasedOnAge').get(() => [{ from: 40, to: 50, then: 1.3 }])
+
+    const expected = carService.currencyFormat.format(244.4)
+    const result = carService.calculateFinalPrice(customer, carCategory, numberOfDays)
+
+    expect(result).to.be.deep.equal(expected)
+  })
+
+  it('given a customer and a car category it should return a transaction receipt', async () => {
+    const car = { ...mocks.validCar }
+
+    const carCategory = { ...mocks.validCarCategory, price: 37.6, carIds: [car.id] }
+    const customer = { ...mocks.validCustomer, age: 20 }
+
+    const numberOfDays = 5
+    
+    const dueDate = '10 de novembro de 2020'
+
+    const now = new Date(2020, 10, 5)
+    sandBox.useFakeTimers(now.getTime())
+
+    sandBox.stub(
+      carService.carRepository,
+      carService.carRepository.find.name
+    ).resolves(car)
+
+    // age: 20, tax:1.1, categoryPrice: 37.6
+    // 37.6 * 1.1 = 41.36 * 5 days = 206.8
+    const expectedAmount = carService.currencyFormat.format(206.8)
+    const result = await carService.rent(customer, carCategory, numberOfDays)
+
+    const expected = new Transaction({
+      customer, 
+      car,
+      dueDate,
+      amount: expectedAmount
+    })
+
     expect(result).to.be.deep.equal(expected)
   })
 })
